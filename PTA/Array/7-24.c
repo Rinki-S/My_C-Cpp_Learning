bc731c8c79f07a3033b3fcf0580572a8de83cec2
riiiinki <nichijousong08@gmail.com>
2023-12-10 19:21:28 +0800

    ✨ Finished

--- PTA/Array/7-24.c
+++ PTA/Array/7-24.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void swap(char *p, char *q) {
     char temp = *p;
@@ -9,27 +10,28 @@ void swap(char *p, char *q) {
 int main(void) {
     char string[80];
     fgets(string, 80, stdin);
-    int ascii[128];
-    for (int i = 0; i < 128; i++) {
-        ascii[i] = 0;
-    }
+    string[strlen(string) - 1] = '\0';
+    int ascii[128] = {0};
+    char result[80] = "";
+    int index = 0;
+
     for (int i = 0; string[i] != '\0'; i++) {
-        if (ascii[string[i]] != 0) {
-            for (int j = i; string[j] != '\0'; j++) {
-                string[j] = string[j + 1];
-            }
-        }
         ascii[string[i]]++;
+        if (ascii[string[i]] > 1) {
+            ascii[string[i]]--;
+        } else {
+            result[index++] = string[i];
+        }
     }
-    for (int i = 0; string[i] != '\0'; i++) {
-        char min = i;
-        for (int j = i; string[j] != '\0'; j++) {
-            if (string[j] < string[min]) {
-                min = j;
+
+    for (int i = 0; i < index - 1; i++) {
+        for (int j = i + 1; j < index; j++) {
+            if (result[i] > result[j]) {
+                swap(&result[i], &result[j]);
             }
         }
-        swap(&string[i], &string[min]);
     }
-    printf("%s", string);
+
+    printf("%s", result);
     return 0;
 }
